dba0f9c2b8324ffc9a259d16a85e5cc3887709e2
Gajendra <gajendra.g@cisinlabs.com>
2015-07-22 15:45:39 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -11,3 +11,21 @@ console.log("This is server file....");
 console.log("This is server file....");
 console.log("This is server file....");
 console.log("*****************-------************************");
+
+
+[{
+  _id: 'sXkBFp6YpAwg4Npmr',
+  processId: 'i98jbpj87Coa4qBDS',
+  connectedAccount: 'N5njQKmMHoBTdMoFh',
+  stepType: 'event',
+  currentEventorActionKey: 'new_push_event',
+  currentServicePackage: 'github',
+  stepProperties: {
+    repo_full_name: 'MahendraNG/myFirstRepo'
+  },
+  stepNumber: 1,
+  isLastStep: false,
+  currentStepBoxId: 'abnew_push_event_1',
+  category: 'integration',
+  packageType: 'API'
+}]
