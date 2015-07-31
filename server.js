b2b38b68ec98d200d361c5ddcd3736af943568c3
Gajendra <gajendra.g@cisinlabs.com>
2015-07-31 18:53:02 +0530

    changes make in server

--- server.js
+++ server.js
@@ -4,7 +4,3 @@ console.log("This is server file....");
 console.log("This is server file....");
 console.log("This is server file....");
 console.log("This is server file....");
-console.log("This is server file....");
-
-
-
